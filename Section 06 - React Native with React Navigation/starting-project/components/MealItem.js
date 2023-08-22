e7f284a6dc9f1bdfa3c3df926ce47677e1942d68
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2023-08-22 23:26:02 +0200

    Section 06

--- Section 06 - React Native with React Navigation/starting-project/components/MealItem.js
+++ Section 06 - React Native with React Navigation/starting-project/components/MealItem.js
@@ -6,16 +6,26 @@ import {
     StyleSheet,
     Platform,
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { MealDetails } from './MealDetails';
 export const MealItem = ({
+    id,
     title,
     imageUrl,
     duration,
     complexity,
     affordability,
 }) => {
+    const navigation = useNavigation();
+
+    const selectMealItemHandler = () => {
+        navigation.navigate('MealDetail', { mealId: id });
+    };
+
     return (
         <View style={styles.mealItem}>
             <Pressable
+                onPress={selectMealItemHandler}
                 android_ripple={{ color: '#ccc' }}
                 style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
             >
@@ -27,15 +37,11 @@ export const MealItem = ({
                         />
                         <Text style={styles.title}>{title}</Text>
                     </View>
-                    <View style={styles.details}>
-                        <Text style={styles.detailItem}>{duration}m</Text>
-                        <Text style={styles.detailItem}>
-                            {complexity.toUpperCase()}
-                        </Text>
-                        <Text style={styles.detailItem}>
-                            {affordability.toUpperCase()}
-                        </Text>
-                    </View>
+                    <MealDetails
+                        duration={duration}
+                        affordability={affordability}
+                        complexity={complexity}
+                    />
                 </View>
             </Pressable>
         </View>
@@ -75,17 +81,5 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 18,
         margin: 8,
-    },
-
-    details: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 8,
-    },
-
-    detailItem: {
-        marginHorizontal: 4,
-        fontSize: 12,
     },
 });
