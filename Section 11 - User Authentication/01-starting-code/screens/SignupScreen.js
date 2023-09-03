48240b3cf1c3cf631cfbfddd411d88d3928d08f4
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2023-09-03 23:03:29 +0200

    Section 11

--- Section 11 - User Authentication/01-starting-code/screens/SignupScreen.js
+++ Section 11 - User Authentication/01-starting-code/screens/SignupScreen.js
@@ -1,7 +1,31 @@
+import { useState } from 'react';
+
 import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { createUser } from '../util/auth';
+import { Alert } from 'react-native';
 
 function SignupScreen() {
-  return <AuthContent />;
+    const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+    async function signupHandler({ email, password }) {
+        setIsAuthenticating(true);
+        try {
+            await createUser(email, password);
+        } catch (error) {
+            Alert.alert(
+                'Authentication Failed',
+                'Could not create user, please check your input and try again later.'
+            );
+        }
+        setIsAuthenticating(false);
+    }
+
+    if (isAuthenticating) {
+        return <LoadingOverlay message='Creating user...' />;
+    }
+
+    return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
--- Section 11 - User Authentication/01-starting-code/util/auth.js
+++ Section 11 - User Authentication/01-starting-code/util/auth.js
@@ -1,13 +1,21 @@
 import axios from 'axios';
-import 'dotenv/config';
+import { API_KEY } from '@env';
 
-async function createUSer(email, password) {
-    const response = await axios.post(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.API_KEY}`,
-        {
-            email: email,
-            password: password,
-            returnSecureToken: true,
-        }
-    );
+async function authenticate(mode, email, password) {
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+    const response = await axios.post(url, {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+    });
+
+    console.log(response.data);
+}
+
+export async function createUser(email, password) {
+    await authenticate('signUp', email, password);
+}
+
+export async function login(email, password) {
+    await authenticate('signInWithPassword', email, password);
 }
